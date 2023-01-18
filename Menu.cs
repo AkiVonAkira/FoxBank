15f8b833808320f64e6121244e99d52a101903eb
AkiVonAkira <akivonakira@gmail.com>
2023-01-18 13:00:23 +0100

    added reusable menu

---
+++ Menu.cs
@@ -0,0 +1,58 @@
+﻿namespace FoxBank
+{
+    internal class Menu
+    {
+        private readonly string[] _menuItems;
+        private int _selectedIndex;
+
+        public Menu(string[] items)
+        {
+            _menuItems = items;
+            _selectedIndex = 0;
+        }
+
+        public void PrintMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("Please select an option:");
+            for (int i = 0; i < _menuItems.Length; i++)
+            {
+                Console.ForegroundColor = i == _selectedIndex ? ConsoleColor.Cyan : ConsoleColor.White;
+                Console.WriteLine(i == _selectedIndex ? $"↪ {_menuItems[i]}" : $"  {_menuItems[i]}  ");
+            }
+            Console.ResetColor();
+        }
+
+        public int SelectIndex
+        {
+            get => _selectedIndex;
+            set => _selectedIndex = (value % _menuItems.Length + _menuItems.Length) % _menuItems.Length;
+        }
+
+        public int UseMenu()
+        {
+            ConsoleKey userInput;
+            do
+            {
+                userInput = Console.ReadKey(true).Key;
+                switch (userInput)
+                {
+                    case ConsoleKey.UpArrow:
+                        _selectedIndex--;
+                        _selectedIndex = (_selectedIndex % _menuItems.Length + _menuItems.Length) % _menuItems.Length;
+                        break;
+                    case ConsoleKey.DownArrow:
+                        _selectedIndex++;
+                        _selectedIndex = (_selectedIndex % _menuItems.Length + _menuItems.Length) % _menuItems.Length;
+                        break;
+                    case ConsoleKey.Enter:
+                    case ConsoleKey.Spacebar:
+                        var index = _selectedIndex;
+                        PrintMenu();
+                        return index;
+                }
+                PrintMenu();
+            } while (true);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,6 +3,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Menu mainMenu = new Menu(new string[] { "Sign In", "Create New User", "Exit" });
+        mainMenu.PrintMenu();
+
+        bool showMenu = true;
+
+        while (showMenu)
+        {
+            int index = mainMenu.UseMenu();
+            switch (index)
+            {
+                case 0:
+                    RandomMethod();
+                    break;
+                case 1:
+                    RandomMethod();
+                    break;
+                case 2:
+                    showMenu = false;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    private static void RandomMethod()
+    {
+        Console.WriteLine("AAAAAAAAAA");
     }
 }
