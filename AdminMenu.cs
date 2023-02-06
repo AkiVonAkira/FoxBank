cb0312c5db3f801fd778617fae6359f0e7b747e7
AkiVonAkira <akivonakira@gmail.com>
2023-02-06 16:23:23 +0100

    account template and better indexer
    Updated indexer to add a go back if you pass on a true boolean !! It does not handle the output well if there is no if check AFTER the MenuIndexer Method !!
    Added AccountTemplate that has default account names and interest rates hard coded. thanks to juiceghost! had the idea of having it in the database but we decided to hard code it instead.

--- AdminMenu.cs
+++ AdminMenu.cs
@@ -32,20 +32,18 @@
 
             // Load role from db, select role names, and turn it into an array.
             List<BankRoleModel> roles = PostgresDataAccess.LoadBankRoleModel();
-            string[] roleArray = roles.Select(role => role.name).ToArray();
+            string[] roleArray = roles.Select(role => role.name.ToUpper()).ToArray();
 
             // menu stuff
-            int roleIndex = Helper.MenuIndexer(roleArray);
+            int roleIndex = Helper.MenuIndexer(roleArray, false);
             int roleId = roles[roleIndex].id;
-
-            Console.ReadLine();
 
             // Load branch from db, select branch names, and turn it into an array.
             List<BankBranchModel> branches = PostgresDataAccess.LoadBankBranchModel();
-            string[] branchArray = branches.Select(branch => branch.name).ToArray();
+            string[] branchArray = branches.Select(branch => branch.name.ToUpper()).ToArray();
 
             // menu stuff
-            int branchIndex = Helper.MenuIndexer(branchArray);
+            int branchIndex = Helper.MenuIndexer(branchArray, false);
             int branchId = branches[branchIndex].id;
 
             Console.Write($"\nCreating User");
