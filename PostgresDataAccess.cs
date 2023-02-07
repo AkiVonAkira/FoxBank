6fd237787a2c7fbc9a37b20257b82a6d4d9933aa
unknown <rohnson95@gmail.com>
2023-02-07 15:04:57 +0100

    added feature to transfer to others

--- PostgresDataAccess.cs
+++ PostgresDataAccess.cs
@@ -91,6 +91,31 @@ public class PostgresDataAccess
             return true;
         }
     }
+    public static bool MoneyTransferOther(int from_account_id, int to_account_id, decimal amount)
+    {
+        using (IDbConnection cnn = new NpgsqlConnection(LoadConnectionString()))
+        {
+            try
+            {
+                var output = cnn.Query($@"
+                BEGIN TRANSACTION;
+                UPDATE bank_account SET balance = CASE
+                    WHEN id = {from_account_id} AND balance >= {amount} THEN balance - {amount}
+                    WHEN id = {to_account_id} THEN balance + {amount}                   
+                END
+                WHERE id IN ({from_account_id},{to_account_id});
+                INSERT INTO bank_transaction (name, from_account_id, to_account_id,amount) VALUES('Överföring',{from_account_id},{to_account_id},{amount});
+                COMMIT;", new DynamicParameters());
+            }
+            catch (Npgsql.PostgresException e)
+            {
+                Console.WriteLine(e.MessageText);
+                return false;
+            }
+            return true;
+        }
+    }
+
 
     public static bool AccountWithdraw(int from_account_id, decimal amount)
     {
